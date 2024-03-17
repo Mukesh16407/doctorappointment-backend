c68b7e8466fa4dbd7cc94553a9743e3881360423
Mukesh16407 <mukesh.16407@gmail.com>
2024-03-17 14:57:19 +0530

    feedback fixes

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const cors = require("cors");
 const app = express();
 const connect = require("./config/dbconfig.js");
 require("dotenv").config();
-const path = require("path");
-
-app.use(express.json());
-app.use(cors());
-
 const userRoute = require("./routes/userRoutes.js");
 const adminRoute = require("./routes/adminRoutes");
 const doctorRoute = require("./routes/doctorRoutes");
 
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
-
-const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
